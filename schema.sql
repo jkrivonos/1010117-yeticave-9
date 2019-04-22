4c24e01bb1236110719bba80d04d5add16f1ad32
KrivonosMS <MS.Krivonos@gamil.com>
2019-04-22 23:28:27 +0300

    добавлена схема базы данных

--- schema.sql
+++ schema.sql
@@ -6,29 +6,34 @@ USE yeticave;
 
 CREATE TABLE category(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(25)
-
+  name CHAR(25),
+  code CHAR(50)
 )
 
 CREATE TABLE lot(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_creation TIMESTAMP,
-  name CHAR(50),
+  creation_date TIMESTAMP,
   description CHAR(50),
-  preview_link CHAR(100),
+  img_link CHAR(100),
   start_price INT,
   final_price INT,
-  delta INT
+  delta_bet INT,
+  category_id INT,
+  user_id INT,
+  winner_user_id INT
 )
 
 CREATE TABLE bet(
-  date TIMESTAMP,
-  ready_price INT
+  id INT,
+  user_id INT,
+  lot_id INT,
+  creation_time TIMESTAMP,
+  price INT
 )
 
-CREATE TABLE users(
+CREATE TABLE user(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_registration TIMESTAMP,
+  registration_date TIMESTAMP,
   email CHAR(50),
   name CHAR(50),
   password CHAR(100),
