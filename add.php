5b9c527cc7fb79a9a8d0f68667ba01000cf0acc5
KrivonosMS <MS.Krivonos@gamil.com>
2019-05-04 16:49:14 +0300

    страница добавления лота

---
+++ add.php
@@ -0,0 +1,24 @@
+<?php
+require 'helpers.php';
+$con = mysqli_connect("localhost", "root", "", "yeticave");
+
+
+
+    $sql = "SELECT name FROM category;";
+    $result = mysqli_query($con, $sql);
+    if (!$result){
+        $error = mysqli_error($con);
+        print("ошибка MySQL:" . $error);
+        die();
+    }
+    $categories_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    var_dump($categories_list);
+
+
+$layout = include_template('layout_add.php', [
+    'categories_list' => $categories_list
+]);
+
+
+print($layout);
+?>
