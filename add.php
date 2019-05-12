ecec2eb571885a5ec2439a05b2ff451cb9c258e9
KrivonosMS <MS.Krivonos@gamil.com>
2019-05-13 00:44:11 +0300

    лот добавляется в БД и после этого редиректит на страницу лота с добавленными пользователем данными. Картинка не подгружается. Нужен фикс.

--- add.php
+++ add.php
@@ -3,122 +3,108 @@ require_once 'helpers.php';
 require_once 'functions.php';
 
 $con = mysqli_connect("localhost", "root", "", "yeticave");
-    $sql = "SELECT name FROM category;";
-    $result = mysqli_query($con, $sql);
-    if (!$result){
-        $error = mysqli_error($con);
-        print("ошибка MySQL:" . $error);
-        die();
-    }
-    $categories_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$sql = "SELECT name FROM category;";
+$result = mysqli_query($con, $sql);
+if (!$result) {
+    $error = mysqli_error($con);
+    print("ошибка MySQL:" . $error);
+    die();
+}
+$categories_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $formData = $_POST;
-        $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
-        $errors = [];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $formData = $_POST;
+    $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+    $errors = [];
 
-        foreach ($required_fields as $field){
-            if (empty($formData[$field])){
-                $errors[$field] = "Поле необходимо заполнить";
-            };
+    foreach ($required_fields as $field) {
+        if (empty($formData[$field])) {
+            $errors[$field] = "Поле необходимо заполнить";
         };
-        $checkedDate =  count($formData) == 0 ?  '' : $formData['lot-date'];
+    };
+    $checkedDate = count($formData) == 0 ? '' : $formData['lot-date'];
 //Проверим, был ли загружен файл.
 // Поле для загрузки файла в форме называется 'img_lot', поэтому нам следует искать в массиве $_FILES одноименный ключ.
 // Если таковой найден, то мы можем получить имя загруженного файла.
 
-        if (isset($_FILES['img_lot']['name'])){
-            $tmp_name = $_FILES['img_lot']['tmp_name'];
-            var_dump($tmp_name);
-            $path = $_FILES['img_lot']['name'];
-            var_dump($path);
+    if (isset($_FILES['img_lot']['name'])) {
+        $tmp_name = $_FILES['img_lot']['tmp_name'];
+//        var_dump($tmp_name);
+        $path = $_FILES['img_lot']['name'];
+//        var_dump($path);
 
 //      С помощью mime_content_type можно получить информацию о типе файле; определяем его расширение
-            $file_name = $_FILES['img_lot']['name'];
-            $file_extantion = substr($file_name, strrpos($file_name, '.')+1);
-            var_dump($file_extantion);
+        $file_name = $_FILES['img_lot']['name'];
+        $file_extantion = substr($file_name, strrpos($file_name, '.') + 1);
+//        var_dump($file_extantion);
 
 
-            $file_type = $tmp_name != '' ? mime_content_type($tmp_name) : '';
-            if ($file_type !=="image/jpg" and $file_type !== "image/jpeg" and $file_type !== "image/png"){
-                $errors['file'] = 'Неверный формат. Загрузите картинку в формате jpg, jpeg, png';
-            }
-            else{
+        $file_type = $tmp_name != '' ? mime_content_type($tmp_name) : '';
+        if ($file_type !== "image/jpg" and $file_type !== "image/jpeg" and $file_type !== "image/png") {
+            $errors['file'] = 'Неверный формат. Загрузите картинку в формате jpg, jpeg, png';
+        } else {
 //          Если файл соответствует ожидаемому типу, то мы копируем его в директорию где лежат все картинки,
 //          а также добавляем путь к загруженной картинки в массив $formData
-                $filename = uniqid() . "." . "$file_extantion";
-                var_dump($filename);
-                var_dump($tmp_name);
-
-                move_uploaded_file($tmp_name, 'uploads/'.$path);
-                $formData['path'] = $filename;
-                var_dump($formData['path']);
-                $userIDRandom = '5';
-                $categoryNameDB = $formData['category'];
-                $sqlIDCategory = 'SELECT id FROM category WHERE name = "'.$categoryNameDB.'"';
-//                $sqlIDCategory = 'SELECT id FROM category WHERE name = "Медикаменты"';
-                $resultIDCategory = mysqli_query($con, $sqlIDCategory);
-                if (!$resultIDCategory){
-                    $error = mysqli_error($con);
-                    print("ошибка MySQL:" . $error);
-                    die();
-                }
-                else{
-                    $idCategory = mysqli_fetch_assoc($resultIDCategory);
-                }
-                    $sql = 'INSERT INTO lot (description, title, creation_date, start_price, expiration_date, delta_bet, img_link, user_id, category_id) VALUES (?, ?, NOW(), ?, ?, ?, ?, 1, ?)';
-                    $stmt = db_get_prepare_stmt($con, $sql, [
-                        $formData['message'],
-                        $formData['lot-name'],
-                        $formData['lot-rate'],
-                        $formData['lot-date'],
-                        $formData['lot-step'],
-                        $formData['path'],
-                        $idCategory['id']
-                ]);
-                    $result = mysqli_stmt_execute($stmt);
-                    if ($result){
-                        echo('добавлено в БД');
-                    }else{
-                        echo('не добавлено!');
-                    }
-            }
-        }else{
-            echo('файла нет');
-            $errors['file'] = 'Вы не загрузили файл';
-        }
-        $isDateValid = isValidDate($checkedDate);
-
-        if (count($errors)){
-            echo('ошибка валидации');
-            $layout = include_template('layout_add.php', [
-                'formData' => $formData,
-                'categories_list' => $categories_list,
-                'errors' => $errors,
-                'isDateValid' => $isDateValid
-            ]);
-        }
-        else {
-            echo('готов записать в бд');
-//        $layout = include_template('lot.php', ['categories_list' => $categories_list]);
-            $layout = include_template('layout_add.php', [
-                'formData' => $formData,
-                'categories_list' => $categories_list,
-                'errors' => $errors,
-                'isDateValid' => $isDateValid
-            ]);
-//            var_dump('===>');
-//            var_dump($_POST);
-//            $lot = $formData['img_lot'];
-//            var_dump($lot);
-//            $filename = uniqid().'.png';
+            $filename = uniqid() . "." . "$file_extantion";
 //            var_dump($filename);
-//            $lot['path'] = $filename;
+//            var_dump($tmp_name);
+
+            move_uploaded_file($tmp_name, 'uploads/' . $path);
+            $formData['path'] = $filename;
+//            var_dump($formData['path']);
+            $userIDRandom = '5';
+            $categoryNameDB = $formData['category'];
+            $sqlIDCategory = 'SELECT id FROM category WHERE name = "' . $categoryNameDB . '"';
+            $resultIDCategory = mysqli_query($con, $sqlIDCategory);
+            if (!$resultIDCategory) {
+                $error = mysqli_error($con);
+                print("ошибка MySQL:" . $error);
+                die();
+            } else {
+                $idCategory = mysqli_fetch_assoc($resultIDCategory);
+            }
+            $sql = 'INSERT INTO lot (description, title, creation_date, start_price, expiration_date, delta_bet, img_link, user_id, category_id) VALUES (?, ?, NOW(), ?, ?, ?, ?, 1, ?)';
+            $stmt = db_get_prepare_stmt($con, $sql, [
+                $formData['message'],
+                $formData['lot-name'],
+                $formData['lot-rate'],
+                $formData['lot-date'],
+                $formData['lot-step'],
+                $formData['path'],
+                $idCategory['id']
+            ]);
+            $result = mysqli_stmt_execute($stmt);
+            if ($result) {
+                $lot_id = mysqli_insert_id($con);
+                header("Location: lot.php?id=" . $lot_id);
+            } else {
+            }
         }
+    } else {
+        echo('файла нет');
+        $errors['file'] = 'Вы не загрузили файл';
     }
-    else{
-        $layout = include_template('layout_add.php', []);
+    $isDateValid = isValidDate($checkedDate);
+
+    if (count($errors)) {
+        echo('ошибка валидации');
+        $layout = include_template('layout_add.php', [
+            'formData' => $formData,
+            'categories_list' => $categories_list,
+            'errors' => $errors,
+            'isDateValid' => $isDateValid
+        ]);
+    } else {
+        $layout = include_template('layout_add.php', [
+            'formData' => $formData,
+            'categories_list' => $categories_list,
+            'errors' => $errors,
+            'isDateValid' => $isDateValid
+        ]);
     }
+} else {
+    $layout = include_template('layout_add.php', []);
+}
 
 print($layout);
 ?>
--- functions.php
+++ functions.php
@@ -1,42 +1,29 @@
 <?php
-    function formatPrice($temp){
-        $formatedPrice = "";
-        $tempPrice = ceil($temp);
-            if ($tempPrice < 1000){
-                $formatedPrice = $tempPrice." "."Р";
-            }else{
-                $formatedPrice = number_format($tempPrice, 0, '.',' ') ." "."Р";
-            }
-        return $formatedPrice;
+function formatPrice($temp)
+{
+    $formatedPrice = "";
+    $tempPrice = ceil($temp);
+    if ($tempPrice < 1000) {
+        $formatedPrice = $tempPrice . " " . "Р";
+    } else {
+        $formatedPrice = number_format($tempPrice, 0, '.', ' ') . " " . "Р";
     }
+    return $formatedPrice;
+}
 
-    function formatTime(){
-        date_default_timezone_set("Europe/Moscow");
-        setlocale(LC_ALL, 'ru_RU');
+function formatTime()
+{
+    date_default_timezone_set("Europe/Moscow");
+    setlocale(LC_ALL, 'ru_RU');
 
-        $interval = (new DateTime("now")) -> diff(new DateTime("tomorrow midnight"));
-        if ($interval->format('%h') < 1) {
-            echo "<div class='timer--finishing timer'>".$interval->format('%h : %i')."</div>";
-        }else{
-            echo "<div class='lot__timer timer'>".$interval->format('%h : %i')."</div>";
-        }
+    $interval = (new DateTime("now"))->diff(new DateTime("tomorrow midnight"));
+    if ($interval->format('%h') < 1) {
+        echo "<div class='timer--finishing timer'>" . $interval->format('%h : %i') . "</div>";
+    } else {
+        echo "<div class='lot__timer timer'>" . $interval->format('%h : %i') . "</div>";
     }
+}
 
-    function isValidDate($date){
-        return preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $date, $m) ? checkdate(intval($m[2]), intval($m[3]), intval($m[1])) : false; }
-?>
-<!---->
-<!--Проверка и проверка YYYY-MM-DD в одной строке-->
-<!---->
-<!--function isValidDate($date) { return preg_match("/^(\d{4})-(\d{1,2})-(\d{1,2})$/", $date, $m) ? checkdate(intval($m[2]), intval($m[3]), intval($m[1])) : false; }-->
-<!---->
-<!--Выход будет:-->
-<!---->
-<!--var_dump(isValidDate("2018-01-01")); // bool(true) var_dump(isValidDate("2018-1-1")); // bool(true) var_dump(isValidDate("2018-02-28")); // bool(true) var_dump(isValidDate("2018-02-30")); // bool(false)-->
-<!---->
-<!--Допускаются день и месяц без начального нуля. Если вы не хотите разрешать это, регулярное выражение должно быть:-->
-<!---->
-<!--"/^(\d{4})-(\d{2})-(\d{2})$/" -->
-
-
-    }?>
+function isValidDate($date){
+    return preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $date, $m) ? checkdate(intval($m[2]), intval($m[3]), intval($m[1])) : false;
+}?>
