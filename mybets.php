dbc767347dee8e6e5652f74c01f2e7de60986ff5
KrivonosMS <MS.Krivonos@gamil.com>
2019-06-26 00:16:54 +0300

    отрендерена страница моих ставок foreach-ом

--- mybets.php
+++ mybets.php
@@ -14,7 +14,21 @@ if (!$result){
 }
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+$sqlBets = "SELECT bet.creation_time, bet.price, lot.title as lot_name, lot.img_link as betImage FROM bet INNER JOIN lot ON bet.lot_id = lot.id";
+
+//"SELECT bet.price, bet.creation_time FROM lot INNER JOIN bet ON bet.lot_id = lot.id WHERE lot.id = 2 ORDER BY bet.creation_time DESC;"
+$resultBets = mysqli_query($con, $sqlBets);
+if (!$resultBets){
+    $error = mysqli_error($con);
+    print("ошибка MySQL:" . $error);
+    die();
+}
+$bets = mysqli_fetch_all($resultBets, MYSQLI_ASSOC);
+
+
+
         $content = include_template('mybets.php', [
+            'bets' => $bets
         ]);
         $user_name = isset($_SESSION['user']) ? $_SESSION['user']['name'] : '';
         $layout= include_template('layout.php',[
